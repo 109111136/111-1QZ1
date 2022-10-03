e418900372a0e1d7501794d2b2ca0629e9fad1e1
109111136 <h226810797@gmail.com>
2022-10-03 11:37:21 +0800

    hi

---
+++ 111-1QZ1/Bomb.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ1
+{
+    public partial class Bomb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            char[,] ca_Map = new char[10, 10];
+            int[] ia_MIndex = new int[10] {0,7, 13, 28, 44, 62, 74, 75, 87, 90};
+            for (int i_Row = 0; i_Row < 10; i_Row++) {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    ca_Map[i_Row, i_Col] = '0';
+                }
+            }
+            for (int i_Ct = 0; i_Ct < 10; i_Ct++) {
+                int i_Row = ia_MIndex[i_Ct] / 10;
+                int i_Col = ia_MIndex[i_Ct] % 10;
+                ca_Map[i_Row, i_Col] = '*';
+            }
+            for (int i_Row = 0; i_Row < 10; i_Row++) {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    Response.Write(ca_Map[i_Row, i_Col]);
+                }
+                    Response.Write("<br/>");
+            }
+        }  
+    }
+}
